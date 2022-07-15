95547e51fd12e0352da79403c6088d41a2e5c546
Arun3sh <m.aruneshwar@gmail.com>
2022-07-16 00:21:41 +0530

    app changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'browsephone';
+  phone:string = "";
+  brand:string = "";
+  image:string = "";
+
+  phoneList:Array<any> = [];
+
+  doChange(value:string){
+    console.warn(value)
+  }
+  getForm(phone:string, brand:string, image:string){
+    this.phoneList.push({phone:phone, brand:brand, image:image})
+    console.warn(phone,brand,image, this.phoneList)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AddPhoneComponent } from './add-phone/add-phone.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AddPhoneComponent
   ],
   imports: [
     BrowserModule,
